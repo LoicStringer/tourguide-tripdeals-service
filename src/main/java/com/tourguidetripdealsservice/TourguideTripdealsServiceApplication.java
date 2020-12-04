34c41ccc07aea21fd5472bd9e58fbdc12968c775
Loïc Stringer <loic.stringer@gmail.com>
2020-12-04 22:39:26 +0100

    Initial commit

---
+++ src/main/java/com/tourguidetripdealsservice/TourguideTripdealsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tourguidetripdealsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourguideTripdealsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourguideTripdealsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourguidetripdealsservice/TourguideTripdealsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourguidetripdealsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourguideTripdealsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

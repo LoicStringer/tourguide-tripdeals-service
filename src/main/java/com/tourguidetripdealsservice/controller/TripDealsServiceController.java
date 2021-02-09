08b872bec63a7d90b1447e52a558480d35f6f384
Loïc Stringer <loic.stringer@gmail.com>
2021-02-09 22:35:35 +0100

    Ready for release

--- src/main/java/com/tourguidetripdealsservice/controller/TripDealsServiceController.java
+++ src/main/java/com/tourguidetripdealsservice/controller/TripDealsServiceController.java
@@ -2,6 +2,8 @@ package com.tourguidetripdealsservice.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +18,14 @@ import com.tourguidetripdealsservice.service.TripDealsService;
 @RestController
 public class TripDealsServiceController {
 	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private TripDealsService tripDealsService;
 
 	@PostMapping("/trip-deals")
 	public ResponseEntity<List<ProviderBean>> getTripDeals (@RequestBody TripPricerDto tripPricerDto) throws TripPricerException{
+		log.info("Querying Trip Pricer external API to get a list of trip deals.");
 		return ResponseEntity.ok(tripDealsService.getTripDeals(tripPricerDto));
 	}
 	
